937c1331e6386f52ba912db42178c1bf4895859e
juliofacal <facaljuliojuanemilio@gmail.com>
2015-03-06 09:19:29 -0300

    prueba
    prueba

---
+++ application/views/templates/scripts.php
@@ -0,0 +1,3 @@
+	<!-- JS Includes -->
+	<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-2.1.1.min.js"); ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
